ce0408db3d32ff7f47f98a78d0e24b0af42badfe
rendisagala <rendisagala19@gmail.com>
2022-01-31 18:31:29 +0700

    add skills section and button for switch mode

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+function switchMode() {
+  const switchIcon = document.querySelector(".switchIcon");
+  const darkMode = document.querySelector(".darkMode");
+
+  switchIcon.classList.toggle("darkMode");
+}
