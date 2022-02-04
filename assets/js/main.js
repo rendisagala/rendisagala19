ed9e96dd09a79d05b2ff12807dad0bb0c844d149
rendisagala <rendisagala19@gmail.com>
2022-02-04 19:56:58 +0700

    fixing script

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,7 +31,6 @@ function switchMode() {
     darkIcon.style.display = "none";
     lightIcon.style.display = "block";
     navLogo.src = "./assets/icon/rasTitle.svg";
-
     // portfolio
 
     portfolioList.classList.remove("light");
